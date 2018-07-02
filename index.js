dd12085a106f34ea764384f9047fcddc45107238
Brennan B <bdbasinger777@gmail.com>
2018-07-02 05:09:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,28 @@ function increaseRankBy(n) {
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (parseInt(i) + n)
+    lis[i].innerHTML = console.log((parseInt(i) + n))
   }
 }
 
+<main id="app">
+      <ul class="unranked-list">
+        <li>2</li>
+        <li>5</li>
+        <li>4</li>
+      </ul>
+
+      <ul class="ranked-list">
+        <li>1</li>
+        <li>2</li>
+      </ul>
+
+      <ul class="ranked-list">
+        <li>12</li>
+        <li>11</li>
+        <li>10</li>
+      </ul>
+      
 
       
       
