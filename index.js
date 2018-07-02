ad7e295cd93f458ae44c6eccb34f17c533a7103b
Brennan B <bdbasinger777@gmail.com>
2018-07-02 05:08:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,24 +16,7 @@ function increaseRankBy(n) {
   }
 }
 
-<main id="app">
-      <ul class="unranked-list">
-        <li>2</li>
-        <li>5</li>
-        <li>4</li>
-      </ul>
 
-      <ul class="ranked-list">
-        <li>1</li>
-        <li>2</li>
-      </ul>
-
-      <ul class="ranked-list">
-        <li>12</li>
-        <li>11</li>
-        <li>10</li>
-      </ul>
-      
       
       
       
